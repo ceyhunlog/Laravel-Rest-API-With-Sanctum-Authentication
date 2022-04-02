fc1f4cc83c572401b4e7a07e320868737a422cfc
ceyhunlog <ceyhunbcelik.log@gmail.com>
2022-04-02 19:51:25 +0300

    using the model

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Product;
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/products', function(){
-    return 'products';
+    return Product::all();
 });
+
+Route::post('/products', function(){
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
+});;
