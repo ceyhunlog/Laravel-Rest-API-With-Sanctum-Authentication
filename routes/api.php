b5a4d57da88c9c4cbf05c75db9887c3e98ca0772
ceyhunlog <ceyhunbcelik.log@gmail.com>
2022-04-02 22:48:26 +0300

    create product and validation

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', function(){
-    return Product::all();
-});
-
-Route::post('/products', function(){
-    return Product::create([
-        'name' => 'Product One',
-        'slug' => 'product-one',
-        'description' => 'This is product one',
-        'price' => '99.99'
-    ]);
-});;
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
